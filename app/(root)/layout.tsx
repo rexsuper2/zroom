638f6044af4e8a32c9ff83326dc981f56d382ebe
rexsuper2 <azharnurfauzi2@gmail.com>
2024-04-07 19:39:08 +0700

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from 'react'
+
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "ZROOM",
+  description: "Video calling app punya azar",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+const RootLayout = ({ children }: { children:ReactNode}) => {
+  return (
+    <main>
+      <StreamVideoProvider>
+        {children}
+      </StreamVideoProvider>
+    </main>
+  )
+}
+
+export default RootLayout
